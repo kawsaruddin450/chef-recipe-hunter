cef45167070501456c857d0ba941b597433224ea
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-08 16:53:21 +0600

    route setup and header section implement

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../Shared/Header/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Page Coming soon</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Shared/Header/Header.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { FaRegUserCircle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='bg-green-400'>
+            <div className="navbar lg:container mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li><a>Home</a></li>
+                            <li><a>Blog</a></li>
+                            <li><a>About Us</a></li>
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">TteokBokki</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Home</a></li>
+                        <li><a>Blog</a></li>
+                        <li><a>About Us</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <FaRegUserCircle className='text-4xl mr-4'></FaRegUserCircle>
+                    <a className="btn btn-neutral">Login</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
