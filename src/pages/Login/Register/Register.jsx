7787beff8e936d56aeefea130d6599ba0e478aa8
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-09 15:35:29 +0600

    implement register page

--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
         <div>
-            <h2>Register Page</h2>
+            <div className="hero bg-base-200 h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Register now!</h1>
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <form className="card-body">
+                            <div className="form-control">
+                                <input type="text" placeholder="name" name='name' id='name' className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <input type="email" placeholder="email" name='email' id='email' className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <input type="password" placeholder="password" name='password' id='password' className="input input-bordered" required />
+                                <label className='label-text-alt'>
+                                    Already have an account? <Link to='/login' className='text-green-300 link link-hover'>Login now</Link>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn bg-green-300 font-bold" type='submit'>Register</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
